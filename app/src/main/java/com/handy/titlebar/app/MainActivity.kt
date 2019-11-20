007c08e8279b6dc6a3f032f56d201ad62294987d
Liu Jie <liujie045.china@outlook.com>
2019-11-20 11:22:32 +0800

    Handy Commit

--- app/src/main/java/com/handy/titlebar/app/MainActivity.kt
+++ app/src/main/java/com/handy/titlebar/app/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.handy.titlebar.app
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnDemo.setOnClickListener {
+            startActivity(Intent(this, DemoActivity::class.java))
+        }
+
+        btnSetting.setOnClickListener {
+            startActivity(Intent(this, SettingActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/handy/titlebar/app/SettingActivity.kt
@@ -0,0 +1,12 @@
+package com.handy.titlebar.app
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @title: SettingActivity
+ * @projectName HandyTitlebar
+ * @description: TODO
+ * @author LiuJie https://github.com/Handy045
+ * @date Created in 2019-11-20 11:05
+ */
+class SettingActivity : AppCompatActivity()
